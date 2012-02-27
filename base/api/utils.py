91ebe09e7b9041bfa90c97b3ddc2cc144add147a
Eetu Purontaus <eetu.purontaus@gmail.com>
2012-02-27 21:07:13 +0200

    Proper JSON encoding, initial filtering for event view

--- base/api/utils.py
+++ base/api/utils.py
@@ -1,20 +1,29 @@
 import re
+from django.core.urlresolvers import reverse
+from models import Event
 
 def events_to_json(events):
-    output = ''
+    output = '{events:['
     
     for event in events:
 	output += obj_to_json(event)
-	
+	output += ','
+
+    output += ']}'
     return output
     
 def obj_to_json(obj):
     output = '{'
-    
+    # Loop through object attributes
     for attr in obj.__dict__:
-	if re.match('_', attr):
+	if re.match('_', attr): # Ignore prefixed with _
 	    continue
+	# Add the rest as name:"value" pairs
 	output += '%s:"%s",' % (attr, obj.__dict__[attr])
-	
+
+    # Class-specific encodings
+    if isinstance(obj, Event) and obj.id:
+	output += 'uri:"%s",' % (reverse('event-detail-view', args=[obj.id]))
+    
     output += '}'
     return output
--- base/api/views.py
+++ base/api/views.py
@@ -1,18 +1,29 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest
 from api.models import Event
 from api.urls import urlpatterns
 from api.utils import events_to_json
+from datetime import datetime
 
 def index(request):
     return HttpResponse('Listing of API functionality here')
     
     
 def list_events(request):
-    events = Event.objects.all()
-    if not events:
-	return HttpResponseNotFound()
+    try:
+	events = Event.objects.all()
+	
+	if 'start' in request.GET:
+	    start_time = datetime.strptime(request.GET['start'], '%Y-%m-%dT%H:%M:%S')
+	    events = events.filter(start_date__gte=start_time)
+	
+	
+	if not events:
+	    return HttpResponseNotFound()
     
-    return HttpResponse(events_to_json(events), mimetype='application/json')
+	return HttpResponse(events_to_json(events), mimetype='application/json')
+	
+    except ValueError as e:
+	return HttpResponseBadRequest(e)
 
 def single_event(request, event_id):
     event = Event.objects.filter(id=event_id)
