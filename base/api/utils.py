7fc4e60f9849e3b3d52dfad7bbd602ad9890002b
Eetu Purontaus <eetu.purontaus@gmail.com>
2012-02-28 15:13:13 +0200

    Working JSON, Ajax loading

--- base/api/utils.py
+++ base/api/utils.py
@@ -3,27 +3,30 @@ from django.core.urlresolvers import reverse
 from models import Event
 
 def events_to_json(events):
-    output = '{events:['
+    output = '{"events":['
     
-    for event in events:
+    for index, event in enumerate(events):
+	if index  > 0: output += ','
 	output += obj_to_json(event)
-	output += ','
 
     output += ']}'
+    
+    #output = '{"events":[{"id":1},{"id":2}]}'
     return output
     
 def obj_to_json(obj):
     output = '{'
     # Loop through object attributes
-    for attr in obj.__dict__:
+    for index, attr in enumerate(obj.__dict__):
 	if re.match('_', attr): # Ignore prefixed with _
 	    continue
 	# Add the rest as name:"value" pairs
-	output += '%s:"%s",' % (attr, obj.__dict__[attr])
+	if index > 0: output += ','
+	output += '"%s":"%s"' % (attr, obj.__dict__[attr])
 
     # Class-specific encodings
     if isinstance(obj, Event) and obj.id:
-	output += 'uri:"%s",' % (reverse('event-detail-view', args=[obj.id]))
+	output += ',"uri":"%s"' % (reverse('event-detail-view', args=[obj.id]))
     
     output += '}'
     return output
--- base/launcher/views.py
+++ base/launcher/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Main launcher page')
+    return HttpResponse('<a href="/events/">Events</a>')
