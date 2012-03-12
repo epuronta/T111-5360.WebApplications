3a9f56f7242d24048714797b9847954012ffb524
Marko Raatikka <marko.raatikka@aalto.fi>
2012-03-13 01:52:36 +0200

    Muutoksia launcherin ulkoasuun ja alustavasti myos events-alisivuun (2/2)

--- base/settings.py
+++ base/settings.py
@@ -75,6 +75,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     #os.path.join(cwd, 'staticfiles'),
+	("scripts", "/static/js"),
 )
 
 # List of finder classes that know how to find static files in
